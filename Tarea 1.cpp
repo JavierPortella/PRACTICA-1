a587547a3334d33d9050c158a4b17574821a0989
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:12:09 -0500

    prueba 1

---
+++ Tarea 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+int a,b,suma;
+printf("escriba primer numero");
+scanf("%d",&a);
+printf("escriba segundo numero");
+scanf("%d",&b);
+suma=a+b;
+printf("suma: %d",suma);
+
+}
